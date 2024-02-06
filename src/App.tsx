2f4162488e1463d9d8d22f21967057203c377105
Matheus <mms.medeiros12@gmail.com>
2024-02-05 21:48:44 -0300

    feat: installing react router dom and adding routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
-function App() {
-  return <div>oi</div>;
-}
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Home from './pages/Home';
+
+const App: React.FC = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/home' element={<Home />} />
+        <Route path='/' element={<Login />} />
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;
