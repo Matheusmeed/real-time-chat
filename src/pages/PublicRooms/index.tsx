6ecb64b7b0eb6d1d4a610fa75d2898248998abe4
Matheus <mms.medeiros12@gmail.com>
2024-02-08 01:00:10 -0300

    feat: adding new favicon, adding countries flags and creating lists of rooms

--- src/pages/PublicRooms/index.tsx
+++ src/pages/PublicRooms/index.tsx
@@ -1,7 +1,128 @@
-import { Wrapper } from './styles';
+import { TextField } from '@material-ui/core';
+import {
+  CardHeader,
+  FilterDiv,
+  RoomCard,
+  RoomsDiv,
+  RoomsSelectionDiv,
+  TitleDiv,
+  Wrapper,
+} from './styles';
+
+const rooms = [
+  {
+    name: 'Bola',
+    category: 'Soccer',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/4/42/Football_in_Bloomington%2C_Indiana%2C_1995.jpg/280px-Football_in_Bloomington%2C_Indiana%2C_1995.jpg',
+    id: 1,
+  },
+  {
+    name: 'Mario',
+    category: 'Games',
+    image:
+      'https://meups.com.br/wp-content/uploads/2023/08/Industria-dos-Games-900x503.jpg',
+    id: 2,
+  },
+  {
+    name: 'Sim',
+    category: 'Games',
+    image:
+      'https://meups.com.br/wp-content/uploads/2023/08/Industria-dos-Games-900x503.jpg',
+    id: 2,
+  },
+  {
+    name: 'Yeah',
+    category: 'Games',
+    image:
+      'https://meups.com.br/wp-content/uploads/2023/08/Industria-dos-Games-900x503.jpg',
+    id: 2,
+  },
+  {
+    name: 'Yeah',
+    category: 'Games',
+    image:
+      'https://meups.com.br/wp-content/uploads/2023/08/Industria-dos-Games-900x503.jpg',
+    id: 2,
+  },
+  {
+    name: 'Yeah',
+    category: 'Games',
+    image:
+      'https://meups.com.br/wp-content/uploads/2023/08/Industria-dos-Games-900x503.jpg',
+    id: 2,
+  },
+  {
+    name: 'Yeah',
+    category: 'Games',
+    image:
+      'https://meups.com.br/wp-content/uploads/2023/08/Industria-dos-Games-900x503.jpg',
+    id: 2,
+  },
+  {
+    name: 'Yeah',
+    category: 'Games',
+    image:
+      'https://meups.com.br/wp-content/uploads/2023/08/Industria-dos-Games-900x503.jpg',
+    id: 2,
+  },
+  {
+    name: 'Yeah',
+    category: 'Games',
+    image:
+      'https://meups.com.br/wp-content/uploads/2023/08/Industria-dos-Games-900x503.jpg',
+    id: 2,
+  },
+  {
+    name: 'Yeah',
+    category: 'Games',
+    image:
+      'https://meups.com.br/wp-content/uploads/2023/08/Industria-dos-Games-900x503.jpg',
+    id: 2,
+  },
+  {
+    name: 'Yeah',
+    category: 'Games',
+    image:
+      'https://meups.com.br/wp-content/uploads/2023/08/Industria-dos-Games-900x503.jpg',
+    id: 2,
+  },
+];
 
 const PublicRooms = () => {
-  return <Wrapper>Public Rooms</Wrapper>;
+  return (
+    <Wrapper>
+      <RoomsSelectionDiv>
+        <FilterDiv>
+          <TextField
+            label='Procurar Sala'
+            variant='standard'
+            type='text'
+            InputLabelProps={{ style: { color: 'white' } }}
+          />
+        </FilterDiv>
+        <RoomsDiv>
+          {rooms.map((room) => (
+            <RoomCard key={room.id} image={room.image}>
+              <CardHeader>
+                <h3>Category</h3>
+                <div>
+                  <img
+                    src='/images/flagIcons/unitedStates.png'
+                    alt='flag'
+                    width={40}
+                  />
+                </div>
+              </CardHeader>
+              <TitleDiv>
+                <h2>Recanto dos Melhores Amigos</h2>
+              </TitleDiv>
+            </RoomCard>
+          ))}
+        </RoomsDiv>
+      </RoomsSelectionDiv>
+    </Wrapper>
+  );
 };
 
 export default PublicRooms;
--- src/pages/PublicRooms/styles.ts
+++ src/pages/PublicRooms/styles.ts
@@ -1,3 +1,128 @@
 import styled from 'styled-components';
 
-export const Wrapper = styled.div``;
+export const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  width: 100%;
+  height: 100vh;
+  background: url('/images/mapBackground.jpg') no-repeat center center fixed;
+  background-size: cover;
+
+  &::before {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: #000000bd;
+  }
+`;
+
+export const RoomsSelectionDiv = styled.div`
+  z-index: 1;
+  width: 1100px;
+  height: 90%;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const FilterDiv = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+export const RoomsDiv = styled.div`
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 20px;
+  margin-top: 30px;
+  overflow-y: auto;
+
+  &::-webkit-scrollbar {
+    width: 8px;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background-color: #816ca4;
+    border-radius: 4px;
+    &:hover {
+      background-color: #967fba;
+    }
+  }
+
+  &::-webkit-scrollbar-track {
+    background-color: #b6b6b614;
+  }
+`;
+
+export const RoomCard = styled.div<{ image: string }>`
+  position: relative;
+  width: 300px;
+  height: 200px;
+  background-image: ${(props) => `url('${props.image}')`};
+  background-size: cover;
+  background-position: center;
+  border: 2px solid #ddd;
+  border-radius: 10px;
+  overflow: hidden;
+  color: #e9e9e9;
+  cursor: pointer;
+  transition: 0.3s;
+
+  &:hover {
+    width: 305px;
+    height: 205px;
+
+    &::after {
+      content: '';
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      background: #9595953b;
+      z-index: 2;
+    }
+  }
+
+  &::before {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: #000000c2;
+    z-index: 1;
+  }
+`;
+
+export const CardHeader = styled.div`
+  position: relative;
+  z-index: 2;
+  display: flex;
+  justify-content: space-between;
+  padding: 10px;
+
+  h3 {
+    margin: 0;
+  }
+`;
+
+export const TitleDiv = styled.div`
+  z-index: 2;
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+
+  h2 {
+    text-align: center;
+  }
+`;
